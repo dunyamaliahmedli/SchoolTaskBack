fa41dcc3fd99e70346f6b576076e52acea3abfe4
Dunyamali Ahmadli <tu7bg7w6d@aztu.local>
2022-05-23 17:18:50 +0400

    added task file

---
+++ WebApplication1/WebApplication1/Models/Slider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string UrlText { get; set; }
+        public string Image { get; set; }
+    }
+}
